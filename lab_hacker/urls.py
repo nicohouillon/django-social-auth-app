851c5ca49a07c2850ec0c86fc93dcad4139180c6
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-08-21 19:31:55 -0300

    Update syntax according to django version
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- lab_hacker/urls.py
+++ lab_hacker/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from lab_hacker.core import views as core_views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path(r'^$', core_views.home, name='home'),
+    path(r'', core_views.home, name='home'),
     path('admin/', admin.site.urls),
-    path(r'^login/$', auth_views.login, name='login'),
-    path(r'^logout/$', auth_views.logout, name='logout'),
-    path(r'^oauth/', include('social_django.urls', namespace='social')),  #
+    path(r'login/', LoginView.as_view(), name='login'),
+    path(r'logout/', LogoutView.as_view(), name='logout'),
+    path(r'oauth/', include('social_django.urls', namespace='social')),
 ]
