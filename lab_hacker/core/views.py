a3f459305e379179b7a579001664f62a7513f962
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-08-22 00:34:12 -0300

    Add request to get repositories for user
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- lab_hacker/core/views.py
+++ lab_hacker/core/views.py
@@ -1,6 +1,34 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from social_django.models import UserSocialAuth
+import requests
+import json
+
+
+def get_repositories_list(github_login):
+    access_token = github_login['access_token']
+    github_user = github_login['login']
+
+    user_header = {
+                   'Authorization': access_token,
+                   'Content-Type': 'application/json'
+                  }
+
+    response = requests.get('https://api.github.com/users/{}/repos'.format(github_user))
+    repositories_list = response.json()
+
+    return repositories_list
 
 @login_required
 def home(request):
-    return render(request, 'core/home.html')
+    user = request.user
+
+    try:
+        github_login = user.social_auth.get(provider='github')
+    except UserSocialAuth.DoesNotExist:
+        github_login = None
+
+    repositories_list = get_repositories_list(github_login.extra_data)
+
+    return render(request, 'core/home.html', {'github_login': github_login,
+                                              'repositories_list': repositories_list})
