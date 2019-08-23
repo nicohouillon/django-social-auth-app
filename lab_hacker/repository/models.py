b2bb80043c0fcac2ff9d0a158c534955ee65440d
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-08-23 18:49:45 -0300

    Create model 'Tag'
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- lab_hacker/repository/models.py
+++ lab_hacker/repository/models.py
@@ -4,8 +4,30 @@ from lab_hacker.user.models import User
 
 class Repository(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
     description = models.TextField()
 
     def __str__(self):
         return self.name
+
+class Tag(models.Model):
+
+    title = models.CharField(
+        _('Tag Tittle'),
+        max_length=30,
+        choices=AWARDS_CHOICES,
+        default=AWARDS_CHOICES[0][0],
+        help_text=_('Ex: Melhor Jogo.')
+    )
+
+    color = models.CharField(
+        _('Colocação'),
+        max_length=30,
+        choices=PLACE_AWARDS_CHOICES,
+        default=PLACE_AWARDS_CHOICES[0][0],
+        help_text=_('Colocação que o jogo ganhou.')
+    )
+
+    def __str__(self):
+        return self.name
+
