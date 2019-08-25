e223f10b5e4036e7e2d89792afe6619c09f86c7d
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-08-25 00:13:10 -0300

    Add repositories methods to respective view
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- lab_hacker/core/views.py
+++ lab_hacker/core/views.py
@@ -1,43 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from social_django.models import UserSocialAuth
-import requests
-import json
 from lab_hacker.repository.models import Repository
 from lab_hacker.repository.tables import RepositoryTable
 from django_tables2 import RequestConfig
 
-
-def instantiate_repository(repository, user):
-    name = repository['name']
-    description = repository['description'] or ""
-
-    obj, created = Repository.objects.update_or_create(
-        name=name, owner=user,
-        defaults={'description': description}
-    )
-
-def create_repositories(repositories_list, user):
-    for repository in repositories_list:
-        print("\n\n\n\n")
-        print(repository)
-        print("\n\n\n\n")
-
-        instantiate_repository(repository, user)
-
-def get_repositories_list(github_login):
-    access_token = github_login['access_token']
-    github_user = github_login['login']
-
-    user_header = {
-                   'Authorization': access_token,
-                   'Content-Type': 'application/json'
-                  }
-
-    response = requests.get('https://api.github.com/users/{}/repos'.format(github_user))
-    repositories_list = response.json()
-
-    return repositories_list
 
 @login_required
 def home(request):
@@ -48,14 +15,12 @@ def home(request):
     except UserSocialAuth.DoesNotExist:
         github_login = None
 
-    #repositories_list = get_repositories_list(github_login.extra_data)
-    #create_repositories(repositories_list, user)
+    tags_query_parameter = request.GET.get('tags')
 
-    repositories_list = Repository.objects.filter(owner=user)
+    query_result = Repository.objects.all()
 
     repositories_table = RepositoryTable(Repository.objects.all())
     RequestConfig(request).configure(repositories_table)
 
     return render(request, 'core/home.html', {'github_login': github_login,
-                                              'repositories_list': repositories_list,
                                               'repositories_table': repositories_table})
--- lab_hacker/repository/views.py
+++ lab_hacker/repository/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from lab_hacker.repository.models import Repository
+import requests
+import json
 
-# Create your views here.
+
+def instantiate_repository(repository, user):
+    name = repository['name']
+    description = repository['description'] or ""
+
+    obj, created = Repository.objects.update_or_create(
+        name=name, owner=user,
+        defaults={'description': description}
+    )
+
+def create_repositories(repositories_list, user):
+    for repository in repositories_list:
+        instantiate_repository(repository, user)
+
+@login_required
+def get_repositories(request):
+    user = request.user
+
+    try:
+        github_login = user.social_auth.get(provider='github')
+    except UserSocialAuth.DoesNotExist:
+        github_login = None
+
+
+    access_token = github_login.extra_data['access_token']
+    github_user = github_login.extra_data['login']
+
+    user_header = {
+                   'Authorization': access_token,
+                   'Content-Type': 'application/json'
+                  }
+
+    response = requests.get('https://api.github.com/users/{}/repos'.format(github_user))
+    repositories_list = response.json()
+
+    create_repositories(repositories_list, user)
+
+    response = redirect('/')
+    return response
